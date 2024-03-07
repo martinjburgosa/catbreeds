492cbafb1add70533e32aac4b19064f3cb1bf796
Jcogollo12 <martinjburgosa@gmail.com>
2024-03-06 20:47:56 -0500

    [Mb] - Se agregan Dtos de aplicación

---
+++ lib/src/application/dtos/cat_info_dto.dart
@@ -0,0 +1,135 @@
+import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'weight_dto.dart';
+
+part 'cat_info_dto.g.dart';
+
+@JsonSerializable()
+class CatInfoDto extends Equatable {
+  const CatInfoDto({
+    required this.weight,
+    required this.id,
+    required this.name,
+    required this.cfaUrl,
+    required this.vetstreetUrl,
+    required this.vcahospitalsUrl,
+    required this.temperament,
+    required this.origin,
+    required this.countryCodes,
+    required this.countryCode,
+    required this.description,
+    required this.lifeSpan,
+    required this.indoor,
+    required this.lap,
+    required this.altNames,
+    required this.adaptability,
+    required this.affectionLevel,
+    required this.childFriendly,
+    required this.dogFriendly,
+    required this.energyLevel,
+    required this.grooming,
+    required this.healthIssues,
+    required this.intelligence,
+    required this.sheddingLevel,
+    required this.socialNeeds,
+    required this.strangerFriendly,
+    required this.vocalisation,
+    required this.experimental,
+    required this.hairless,
+    required this.natural,
+    required this.rare,
+    required this.rex,
+    required this.suppressedTail,
+    required this.shortLegs,
+    required this.wikipediaUrl,
+    required this.hypoallergenic,
+    required this.referenceImageId,
+    required this.imageUrl,
+  });
+
+  factory CatInfoDto.fromJson(Map<String, dynamic> json) =>
+      _$CatInfoDtoFromJson(json);
+
+  final WeightDto weight;
+  final String id;
+  final String name;
+  final String cfaUrl;
+  final String vetstreetUrl;
+  final String vcahospitalsUrl;
+  final String temperament;
+  final String origin;
+  final String countryCodes;
+  final String countryCode;
+  final String description;
+  final String lifeSpan;
+  final int indoor;
+  final int lap;
+  final String altNames;
+  final int adaptability;
+  final int affectionLevel;
+  final int childFriendly;
+  final int dogFriendly;
+  final int energyLevel;
+  final int grooming;
+  final int healthIssues;
+  final int intelligence;
+  final int sheddingLevel;
+  final int socialNeeds;
+  final int strangerFriendly;
+  final int vocalisation;
+  final int experimental;
+  final int hairless;
+  final int natural;
+  final int rare;
+  final int rex;
+  final int suppressedTail;
+  final int shortLegs;
+  final String wikipediaUrl;
+  final int hypoallergenic;
+  final String referenceImageId;
+  final String imageUrl;
+
+  Map<String, dynamic> toJson() => _$CatInfoDtoToJson(this);
+
+  @override
+  List<Object?> get props => [
+        id,
+        name,
+        cfaUrl,
+        imageUrl,
+        vetstreetUrl,
+        vcahospitalsUrl,
+        temperament,
+        origin,
+        countryCodes,
+        countryCode,
+        description,
+        lifeSpan,
+        indoor,
+        lap,
+        altNames,
+        adaptability,
+        affectionLevel,
+        childFriendly,
+        dogFriendly,
+        energyLevel,
+        grooming,
+        healthIssues,
+        intelligence,
+        sheddingLevel,
+        socialNeeds,
+        strangerFriendly,
+        vocalisation,
+        experimental,
+        hairless,
+        natural,
+        rare,
+        rex,
+        suppressedTail,
+        shortLegs,
+        wikipediaUrl,
+        hypoallergenic,
+        referenceImageId,
+      ];
+}
---
+++ lib/src/application/dtos/weight_dto.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'weight_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+WeightDto _$WeightDtoFromJson(Map<String, dynamic> json) => WeightDto(
+      imperial: json['imperial'] as String,
+      metric: json['metric'] as String,
+    );
+
+Map<String, dynamic> _$WeightDtoToJson(WeightDto instance) => <String, dynamic>{
+      'imperial': instance.imperial,
+      'metric': instance.metric,
+    };
