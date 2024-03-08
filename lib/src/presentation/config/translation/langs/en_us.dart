be360c1ffa856bb259bca1f955b87bb9ebe3cfe0
Jcogollo12 <martinjburgosa@gmail.com>
2024-03-08 02:35:33 -0500

    [Mb] - Se agregan labels de internacionalización

--- lib/src/presentation/config/translation/langs/en_us.dart
+++ lib/src/presentation/config/translation/langs/en_us.dart
@@ -1,2 +1,12 @@
 /// Set of translations to en_US locale.
-const Map<String, String> enUs = {'lbl_layout_title': 'Catbreeds App'};
+const Map<String, String> enUs = {
+  'lbl_layout_title': 'Catbreeds App',
+  'lbl_home_card_more': 'More',
+  'lbl_detail_description': 'Description',
+  'lbl_detail_origin_country': 'Origin country',
+  'lbl_detail_life_span': 'Life span',
+  'lbl_detail_dog_friendly': 'Dog friendly',
+  'lbl_detail_intelligence': 'Intelligence',
+  'lbl_detail_child_friendly': 'Child friendly',
+  'lbl_detail_energy_level': 'Energy level',
+};
